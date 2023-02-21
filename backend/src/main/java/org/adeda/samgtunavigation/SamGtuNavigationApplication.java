bbb81156d53d26cf1426f60f3013a76eb276c19a
arsuhinars <arsuhinars@gmail.com>
2023-02-21 17:18:14 +0400

    🎉 Created backend project

---
+++ backend/src/main/java/org/adeda/samgtunavigation/SamGtuNavigationApplication.java
@@ -0,0 +1,13 @@
+package org.adeda.samgtunavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamGtuNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamGtuNavigationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/adeda/samgtunavigation/SamGtuNavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adeda.samgtunavigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamGtuNavigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
