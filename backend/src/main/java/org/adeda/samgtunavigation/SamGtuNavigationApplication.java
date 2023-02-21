c80e105dea4a580002875bd0c99eb6b6b2d6947b
arsuhinars <arsuhinars@gmail.com>
2023-02-21 17:54:40 +0400

    🐛 Fixed Spring app class to work without Data Source

--- backend/src/main/java/org/adeda/samgtunavigation/SamGtuNavigationApplication.java
+++ backend/src/main/java/org/adeda/samgtunavigation/SamGtuNavigationApplication.java
@@ -2,8 +2,9 @@ package org.adeda.samgtunavigation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SamGtuNavigationApplication {
 
 	public static void main(String[] args) {
