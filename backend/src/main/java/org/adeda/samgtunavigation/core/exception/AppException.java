871558627dbb767cb6e468a46c31e2f625d7acec
arsuhinars <arsuhinars@gmail.com>
2023-03-15 14:38:47 +0400

    :sparkles: Implemented AppException

--- backend/src/main/java/org/adeda/samgtunavigation/core/exception/AppException.java
+++ backend/src/main/java/org/adeda/samgtunavigation/core/exception/AppException.java
@@ -1,4 +1,18 @@
 package org.adeda.samgtunavigation.core.exception;
 
-public class AppException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class AppException extends RuntimeException {
+    private final String details;
+
+    private final HttpStatus status;
+
+    public AppException(String details, HttpStatus status) {
+        super(details);
+
+        this.details = details;
+        this.status = status;
+    }
 }
