f60c3d458859a80f3ae86196a84260ed6b3939a8
arsuhinars <arsuhinars@gmail.com>
2023-03-15 14:39:05 +0400

    :sparkles: Added InvalidFormatException

---
+++ backend/src/main/java/org/adeda/samgtunavigation/core/exception/InvalidFormatException.java
@@ -0,0 +1,19 @@
+package org.adeda.samgtunavigation.core.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidFormatException extends AppException {
+
+    public InvalidFormatException() {
+        super("Invalid input format", HttpStatus.BAD_REQUEST);
+    }
+
+    public InvalidFormatException(String details) {
+        super(details, HttpStatus.BAD_REQUEST);
+    }
+
+    public InvalidFormatException(String details, HttpStatus status) {
+        super(details, status);
+    }
+}
+/**/
