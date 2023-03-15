3de3a85043f4c357b4533efb945db8d606a04df9
arsuhinars <arsuhinars@gmail.com>
2023-03-15 14:41:39 +0400

    :art: Added base interface for NodeRepository

---
+++ backend/src/main/java/org/adeda/samgtunavigation/navigation/repository/NodeRepository.java
@@ -0,0 +1,8 @@
+package org.adeda.samgtunavigation.navigation.repository;
+
+import org.adeda.samgtunavigation.navigation.model.NavNode;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NodeRepository extends CrudRepository<NavNode, Integer> {
+
+}
