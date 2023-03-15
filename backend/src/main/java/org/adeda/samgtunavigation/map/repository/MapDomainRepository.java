d7a03f9da3dbf352b91ef407c05e7f316d83f174
arsuhinars <arsuhinars@gmail.com>
2023-03-15 14:41:53 +0400

    :sparkles: Added MapDomainRepository

---
+++ backend/src/main/java/org/adeda/samgtunavigation/map/repository/MapDomainRepository.java
@@ -0,0 +1,8 @@
+package org.adeda.samgtunavigation.map.repository;
+
+import org.adeda.samgtunavigation.map.model.MapDomain;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MapDomainRepository extends CrudRepository<MapDomain, Integer> {
+
+}
