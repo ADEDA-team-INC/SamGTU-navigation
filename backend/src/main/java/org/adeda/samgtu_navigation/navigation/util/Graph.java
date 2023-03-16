3e0556403ea1e1e40ee617d342a3e3aefc2f4818
arsuhinars <arsuhinars@gmail.com>
2023-03-16 14:40:31 +0400

    :sparkles: Added methods into Graph interface

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/util/Graph.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/util/Graph.java
@@ -1,5 +1,15 @@
 package org.adeda.samgtu_navigation.navigation.util;
 
-public interface Graph {
+import org.adeda.samgtu_navigation.core.exception.NotFoundException;
+import org.adeda.samgtu_navigation.core.util.Vector2;
+import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
+import java.util.List;
+
+public interface Graph {
+    NavNode findClosestNode(Integer domainId, Vector2 position) throws NotFoundException;
+
+    Double getNodesDistance(NavNode node1, NavNode node2) throws NotFoundException;
+
+    List<NavNode> findPath(NavNode startNode, NavNode endNode) throws NotFoundException;
 }
