66cbb4aad906965280791c9e9b071ed21ab4a103
arsuhinars <arsuhinars@gmail.com>
2023-03-16 14:39:43 +0400

    :sparkles: Added custom queries into MapDomainRepository

--- backend/src/main/java/org/adeda/samgtu_navigation/map/repository/MapDomainRepository.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/repository/MapDomainRepository.java
@@ -3,6 +3,8 @@ package org.adeda.samgtu_navigation.map.repository;
 import org.adeda.samgtu_navigation.map.model.MapDomain;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MapDomainRepository extends CrudRepository<MapDomain, Integer> {
+import java.util.List;
 
+public interface MapDomainRepository extends CrudRepository<MapDomain, Integer> {
+    List<MapDomain> findByBuildingId(Integer id);
 }
