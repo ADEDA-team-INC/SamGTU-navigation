f41a76d35edab526ba97a8849305f40393e35226
arsuhinars <arsuhinars@gmail.com>
2023-03-16 14:40:19 +0400

    :sparkles: Added methods into controllers interfaces

--- backend/src/main/java/org/adeda/samgtu_navigation/map/controller/MapController.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/controller/MapController.java
@@ -1,5 +1,11 @@
 package org.adeda.samgtu_navigation.map.controller;
 
-public interface MapController {
+import org.adeda.samgtu_navigation.core.exception.NotFoundException;
+import org.adeda.samgtu_navigation.map.schema.MapBuildingSchema;
+import org.adeda.samgtu_navigation.map.schema.MapDomainSchema;
 
+public interface MapController {
+    MapBuildingSchema getMapBuildingById(Integer id) throws NotFoundException;
+
+    MapDomainSchema getMapDomainById(Integer id) throws NotFoundException;
 }
--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/controller/NavigationController.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/controller/NavigationController.java
@@ -1,4 +1,11 @@
 package org.adeda.samgtu_navigation.navigation.controller;
 
+import org.adeda.samgtu_navigation.core.exception.NotFoundException;
+import org.adeda.samgtu_navigation.navigation.schema.NavPointSchema;
+import org.adeda.samgtu_navigation.navigation.schema.NavigationRequest;
+
+import java.util.List;
+
 public interface NavigationController {
+    List<NavPointSchema> findPath(NavigationRequest schema) throws NotFoundException;
 }
