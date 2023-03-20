85c7c4d4552d663a335c73c942fd76970ad8c4a9
arsuhinars <arsuhinars@gmail.com>
2023-03-20 20:01:49 +0400

    :bug: Fixed vector class in Graph interface

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/Graph.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/Graph.java
@@ -1,13 +1,13 @@
 package org.adeda.samgtu_navigation.navigation.graph;
 
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
-import org.adeda.samgtu_navigation.core.util.Vector2;
+import org.adeda.samgtu_navigation.core.util.Vector2d;
 import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
 import java.util.List;
 
 public interface Graph {
-    NavNode findClosestNode(Integer domainId, Vector2 position) throws NotFoundException;
+    NavNode findClosestNode(Integer domainId, Vector2d position) throws NotFoundException;
 
     Double getNodesDistance(NavNode node1, NavNode node2) throws NotFoundException;
 
