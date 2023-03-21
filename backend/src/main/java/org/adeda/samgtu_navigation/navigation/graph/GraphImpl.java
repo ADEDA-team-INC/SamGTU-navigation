9f06302a5741ce513050c0fe20b3cf5c31c22065
arsuhinars <arsuhinars@gmail.com>
2023-03-21 23:56:01 +0400

    :art: GraphImpl class update

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/GraphImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/GraphImpl.java
@@ -1,40 +1,23 @@
 package org.adeda.samgtu_navigation.navigation.graph;
 
+import org.adeda.samgtu_navigation.core.exception.InvalidFormatException;
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
-import org.adeda.samgtu_navigation.core.util.Vector2d;
 import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class GraphImpl implements Graph {
-    private final List<GraphNode> nodes;
-    private final GraphField rootField;
-    private final DistanceMatrix distanceMatrix = new DistanceMatrix();
+    private final Set<NavNode> nodes;
+    private final DistanceMatrix distanceMatrix;
 
-    private final Map<NavNode, GraphNode> graphNodeByNavNode = Map.of();
-    private final Map<GraphNode, NavNode> navNodeByGraphNode = Map.of();
-
-    public GraphImpl(List<GraphNode> nodes, GraphField rootField) {
-        this.nodes = Collections.unmodifiableList(nodes);
-        this.rootField = rootField;
-
-        // TODO: построение матрицы расстояний
+    public GraphImpl(Set<NavNode> nodes, DistanceMatrix distanceMatrix) {
+        this.nodes = nodes;
+        this.distanceMatrix = distanceMatrix;
     }
 
     @Override
-    public NavNode findClosestNode(Integer domainId, Vector2d position) throws NotFoundException {
-        return null;
-    }
-
-    @Override
-    public Double getNodesDistance(NavNode node1, NavNode node2) throws NotFoundException {
-        return null;
-    }
-
-    @Override
-    public List<NavNode> findPath(NavNode startNode, NavNode endNode) throws NotFoundException {
-        return null;
+    public List<NavNode> findPath(NavNode startNode, NavNode endNode) throws NotFoundException, InvalidFormatException {
+        throw new UnsupportedOperationException();
     }
 }
