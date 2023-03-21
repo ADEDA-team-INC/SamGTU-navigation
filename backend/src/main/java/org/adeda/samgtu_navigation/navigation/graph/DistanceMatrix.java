584b47fd52a02fb6eab6f68f10127db02974870e
arsuhinars <arsuhinars@gmail.com>
2023-03-21 12:02:35 +0400

    :sparkles: Added DistanceMatrix class

---
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/DistanceMatrix.java
@@ -0,0 +1,9 @@
+package org.adeda.samgtu_navigation.navigation.graph;
+
+import org.adeda.samgtu_navigation.core.util.Pair;
+
+import java.util.HashMap;
+
+public class DistanceMatrix extends HashMap<Pair<GraphNode, GraphNode>, Double> {
+
+}
