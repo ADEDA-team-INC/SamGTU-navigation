af5cf67ae160ddd839d96295524699031781703d
arsuhinars <arsuhinars@gmail.com>
2023-03-21 23:53:37 +0400

    :bulb: Added comments to some methods

--- backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapService.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapService.java
@@ -4,7 +4,18 @@ import org.adeda.samgtu_navigation.map.schema.MapBuildingSchema;
 import org.adeda.samgtu_navigation.map.schema.MapDomainSchema;
 
 public interface MapService {
+    /**
+     * Получения объекта здания на карте
+     * @param id идентификатор здания
+     *
+     * @return Объект здания `MapBuildingSchema` или `null`, если объект не найден.
+     */
     MapBuildingSchema getBuildingById(Integer id);
 
+    /**
+     * Получения объекта пространства на карте
+     * @param id идентификатор пространства
+     * @return Объект пространства `MapDomainSchema` или `null`, если объект не найден.
+     */
     MapDomainSchema getDomainById(Integer id);
 }
--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/NavigationService.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/NavigationService.java
@@ -1,10 +1,20 @@
 package org.adeda.samgtu_navigation.navigation.service;
 
+import org.adeda.samgtu_navigation.core.exception.InvalidFormatException;
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
 import org.adeda.samgtu_navigation.navigation.schema.NavPointSchema;
 
 import java.util.List;
 
 public interface NavigationService {
-    List<NavPointSchema> findPath(NavPointSchema startPoint, NavPointSchema endPoint) throws NotFoundException;
+    /**
+     * Метод поиска пути между заданными точками
+     * @param points список точек, между которыми нужно найти путь.
+     *               Должен содержать минимум 2 элемента.
+     * @return Список найденных точек
+     * @throws NotFoundException если позиция точки не принадлежат предоставленному объекту на карте `mapObjectId`
+     * или путь между ними не существует.
+     * @throws InvalidFormatException если `points` содержит меньше 2 элементов
+     */
+    List<NavPointSchema> findPath(List<NavPointSchema> points) throws NotFoundException, InvalidFormatException;
 }
