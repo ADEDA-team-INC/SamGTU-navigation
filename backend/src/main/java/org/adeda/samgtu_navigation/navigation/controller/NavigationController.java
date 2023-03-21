e88457c6dbef87ae173322a88788f2087f487ea0
arsuhinars <arsuhinars@gmail.com>
2023-03-21 23:55:49 +0400

    :art: Changed NavigationController behaviour

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/controller/NavigationController.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/controller/NavigationController.java
@@ -2,10 +2,9 @@ package org.adeda.samgtu_navigation.navigation.controller;
 
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
 import org.adeda.samgtu_navigation.navigation.schema.NavPointSchema;
-import org.adeda.samgtu_navigation.navigation.schema.NavigationRequest;
 
 import java.util.List;
 
 public interface NavigationController {
-    List<NavPointSchema> findPath(NavigationRequest schema) throws NotFoundException;
+    List<NavPointSchema> findPath(List<NavPointSchema> schema) throws NotFoundException;
 }
