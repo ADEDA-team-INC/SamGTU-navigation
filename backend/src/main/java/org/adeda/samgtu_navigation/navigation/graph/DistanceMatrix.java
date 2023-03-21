6fa7b93cf499ee2035108501bc2c547f660731cd
arsuhinars <arsuhinars@gmail.com>
2023-03-21 23:55:10 +0400

    :bug: Fixed DistanceMatrix class

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/DistanceMatrix.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/DistanceMatrix.java
@@ -1,9 +1,10 @@
 package org.adeda.samgtu_navigation.navigation.graph;
 
 import org.adeda.samgtu_navigation.core.util.Pair;
+import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
 import java.util.HashMap;
 
-public class DistanceMatrix extends HashMap<Pair<GraphNode, GraphNode>, Double> {
+public class DistanceMatrix extends HashMap<Pair<NavNode, NavNode>, Double> {
 
 }
