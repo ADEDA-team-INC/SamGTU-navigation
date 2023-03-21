ecfa7241bc8ab56799404cc7117025c0de780efc
arsuhinars <arsuhinars@gmail.com>
2023-03-21 23:56:23 +0400

    :art: Removed useless methods and added comments in Graph interface

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/Graph.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/Graph.java
@@ -1,15 +1,20 @@
 package org.adeda.samgtu_navigation.navigation.graph;
 
+import org.adeda.samgtu_navigation.core.exception.InvalidFormatException;
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
-import org.adeda.samgtu_navigation.core.util.Vector2d;
 import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
 import java.util.List;
 
 public interface Graph {
-    NavNode findClosestNode(Integer domainId, Vector2d position) throws NotFoundException;
-
-    Double getNodesDistance(NavNode node1, NavNode node2) throws NotFoundException;
-
-    List<NavNode> findPath(NavNode startNode, NavNode endNode) throws NotFoundException;
+    /**
+     * Метод поиска пути между двумя узлами графа. Внутри использует алгортим A*
+     * @param startNode узел начала пути
+     * @param endNode узел конца пути
+     * @return Список узлов, соответствующих найденному пути. В начале и конце
+     * содержит `startNode` и `endNode` соответственно.
+     * @throws NotFoundException если путь между узлами не найден
+     * @throws InvalidFormatException если `startNode` или `endNode` не принадлежат текущему графу
+     */
+    List<NavNode> findPath(NavNode startNode, NavNode endNode) throws NotFoundException, InvalidFormatException;
 }
