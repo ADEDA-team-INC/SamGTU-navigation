5f049354dabe74b5846de23bd63b825fbb1adf35
Вячеслав Чучманов <vaceslavcucmanov@MacBook-Air-Vaceslav.local>
2023-03-22 21:06:46 +0400

    added elements position and domain UI

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Domain } from '../UI/domains/domain'
 import { Zoom } from '../UI/zoom/zoom'
 
 export const MainPage = props => {
     return (
-        <div>
-            <Zoom />
+        <div className='main'>
+            <div className='main__right__container'>
+                <Domain />
+                <Zoom />
+            </div>
         </div>
     )
 }
