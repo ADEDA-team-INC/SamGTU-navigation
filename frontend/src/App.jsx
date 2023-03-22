5a8eb789bb0a22cc0046256b18df303eeb3379a9
Вячеслав Чучманов <vaceslavcucmanov@MacBook-Air-Vaceslav.local>
2023-03-22 21:15:03 +0400

    deleted imports and state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.scss'
 import { MainPage } from './Components/Pages/MainPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <MainPage />
