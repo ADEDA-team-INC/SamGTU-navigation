23ea68ceff07da510facaa48899383c2b32963c3
Вячеслав Чучманов <vaceslavcucmanov@MacBook-Air-Vaceslav.local>
2023-03-23 17:22:00 +0400

    added infobar component

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Infobar } from '../Infobar/Infobar'
 import { Domain } from '../UI/domains/domain'
 import { Zoom } from '../UI/zoom/zoom'
 
 export const MainPage = props => {
     return (
         <div className='main'>
+            <div className='main__top__container'>
+                <Infobar />
+            </div>
             <div className='main__right__container'>
                 <Domain />
                 <Zoom />
