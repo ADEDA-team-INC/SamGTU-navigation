77ef2817d47db16657e8f3db735daf7d70b14756
oliceglad <vycheslavofficial@gmail.com>
2023-03-29 21:34:37 +0400

    added animation for adaptive screen and fix bugs

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import Scroll from '../UI/scroll/Scroll';
 import SearchList from './SearchList';
 import s from "./search.module.scss";
+import pointer from '../../assets/img/point.svg'
+import up from '../../assets/img/up.svg'
 
 function Search({ details }) {
     const [searchField, setSearchField] = useState("");
+    const [ifPointer, setIfPointer] = useState(false);
 
     const filteredBuildings = details.filter(
         building => {
@@ -23,16 +26,25 @@ function Search({ details }) {
 
     const handleChange = e => {
         setSearchField(e.target.value);
+        setIfPointer(false);
     };
 
     return (
-        <section className={s.search}>
+        <section className={!ifPointer ? s.search : s.search__active}>
             <input type="search" className={s.search__input} placeholder="Поиск"
                 onChange={handleChange} />
             <Scroll>
-                <h1 className={s.search__title}>
-                    Результаты:
-                </h1>
+                <div className={s.search__container}>
+                    <h1 className={s.search__title}>
+                        Результаты:
+                    </h1>
+                    <a href="##" onClick={
+                        ifPointer ? () => (setIfPointer(false)) : () => (setIfPointer(true))
+                    } >
+                        <img className={!ifPointer ? s.search__pointer : s.off} src={pointer} />
+                        <img className={ifPointer ? s.search__up : s.off} src={up} />
+                    </a>
+                </div>
                 <>
                     {filteredBuildings.length > 0 ? <SearchList filteredBuildings={filteredBuildings} /> : <div className={s.nothing__text}> Ничего не найдено </div>}
                 </>
