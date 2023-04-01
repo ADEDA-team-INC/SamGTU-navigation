f0b3091b6ee90b64cef9e6be801407e553b6a250
arsuhinars <arsuhinars@gmail.com>
2023-04-01 20:34:20 +0400

    :sparkles: Extended DistanceMatrix class

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/DistanceMatrix.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/DistanceMatrix.java
@@ -5,6 +5,20 @@ import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
 import java.util.HashMap;
 
-public class DistanceMatrix extends HashMap<Pair<NavNode, NavNode>, Double> {
+public class DistanceMatrix {
+    private final HashMap<Pair<NavNode, NavNode>, Double> matrix;
 
+    public DistanceMatrix(HashMap<Pair<NavNode, NavNode>, Double> matrix) {
+        this.matrix = matrix;
+    }
+
+    public double getDistance(NavNode firstNode, NavNode secondNode) {
+        var pair = new Pair<>(firstNode, secondNode);
+
+        if (matrix.containsKey(pair)) {
+            return matrix.get(pair);
+        }
+
+        return matrix.get(pair.swap());
+    }
 }
