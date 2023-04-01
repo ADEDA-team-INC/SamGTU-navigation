49e15ae4b461eb57c0e7e9c8b0c86e6d223858fd
mister-mamaster <burlakop@gmail.com>
2023-04-01 16:33:40 +0400

    Equals for Pair

--- backend/src/main/java/org/adeda/samgtu_navigation/core/util/Pair.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/core/util/Pair.java
@@ -3,9 +3,23 @@ package org.adeda.samgtu_navigation.core.util;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 public class Pair<T, V> {
     private T first;
     private V second;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return (Objects.equals(first, pair.first)
+                    && Objects.equals(second, pair.second))
+                ||
+                (Objects.equals(first, pair.second)
+                    && Objects.equals(second, pair.first));
+    }
 }
