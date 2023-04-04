70b7defdec2444f097199989ea1b049f0ba0c0e9
arsuhinars <arsuhinars@gmail.com>
2023-04-04 22:19:53 +0400

    :art: Added new methods in MapService

--- backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapService.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapService.java
@@ -2,10 +2,20 @@ package org.adeda.samgtu_navigation.map.service;
 
 import org.adeda.samgtu_navigation.map.schema.MapBuildingSchema;
 import org.adeda.samgtu_navigation.map.schema.MapDomainSchema;
+import org.adeda.samgtu_navigation.map.schema.OutdoorObjectSchema;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MapService {
+    /**
+     * Получить все здания на карте. Поддерживает пагинацию
+     * @param size размер страницы
+     * @param page номер страницы
+     * @return Список всех зданий с учетом текущей страницы
+     */
+    List<MapBuildingSchema> getAllBuildings(int size, int page);
+
     /**
      * Получения объекта здания на карте
      * @param id идентификатор здания
@@ -20,4 +30,12 @@ public interface MapService {
      * @return Объект пространства `MapDomainSchema`.
      */
     Optional<MapDomainSchema> getDomainById(Integer id);
+
+    /**
+     * Получить все уличные объекты на карте. Поддерживает пагинацию
+     * @param size размер страницы
+     * @param page номер страницы
+     * @return Список всех уличных объектов с учетом текущей страницы
+     */
+    List<OutdoorObjectSchema> getAllOutdoorObjects(int size, int page);
 }
--- backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapServiceImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapServiceImpl.java
@@ -2,21 +2,35 @@ package org.adeda.samgtu_navigation.map.service;
 
 import org.adeda.samgtu_navigation.map.repository.MapBuildingRepository;
 import org.adeda.samgtu_navigation.map.repository.MapDomainRepository;
+import org.adeda.samgtu_navigation.map.repository.OutdoorObjectRepository;
 import org.adeda.samgtu_navigation.map.schema.MapBuildingSchema;
 import org.adeda.samgtu_navigation.map.schema.MapDomainSchema;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.adeda.samgtu_navigation.map.schema.OutdoorObjectSchema;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
-public class MapServiceImpl implements MapService{
+public class MapServiceImpl implements MapService {
     private final MapBuildingRepository buildingRepository;
     private final MapDomainRepository domainRepository;
+    private final OutdoorObjectRepository outdoorObjectRepository;
 
-    public MapServiceImpl(MapBuildingRepository buildingRepository, MapDomainRepository domainRepository) {
+    public MapServiceImpl(
+        MapBuildingRepository buildingRepository,
+        MapDomainRepository domainRepository,
+        OutdoorObjectRepository outdoorObjectRepository
+    ) {
         this.buildingRepository = buildingRepository;
         this.domainRepository = domainRepository;
+        this.outdoorObjectRepository = outdoorObjectRepository;
+    }
+
+    @Override
+    public List<MapBuildingSchema> getAllBuildings(int size, int page) {
+        return buildingRepository.findAll(PageRequest.of(page, size)).map(MapBuildingSchema::new).toList();
     }
 
     @Override
@@ -27,5 +41,10 @@ public class MapServiceImpl implements MapService{
     @Override
     public Optional<MapDomainSchema> getDomainById(Integer id) {
         return domainRepository.findById(id).map(MapDomainSchema::new);
+    }
+
+    @Override
+    public List<OutdoorObjectSchema> getAllOutdoorObjects(int size, int page) {
+        return outdoorObjectRepository.findAll(PageRequest.of(page, size)).map(OutdoorObjectSchema::new).toList();
     }
 }
