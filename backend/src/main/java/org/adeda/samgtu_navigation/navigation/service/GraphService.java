7ad55b6f213a8253d2f8bfc83db029fed2309d33
arsuhinars <arsuhinars@gmail.com>
2023-04-04 22:18:34 +0400

    :art: GraphSupplier was replaced by GraphService

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphService.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphService.java
@@ -1,15 +1,12 @@
-package org.adeda.samgtu_navigation.navigation.graph;
+package org.adeda.samgtu_navigation.navigation.service;
 
-import org.springframework.stereotype.Component;
+import org.adeda.samgtu_navigation.navigation.graph.Graph;
 
-@Component
-public class GraphSupplier {
+public interface GraphService {
     /**
      * Метод для поставки графа, соответствующему одному зданию на карте.
      * @param buildingId идентификатор здания
      * @return Новый объект графа, созданный с помощью `GraphFactory`, либо уже существующий.
      */
-    public Graph getGraphByBuildingId(Integer buildingId) {
-        throw new UnsupportedOperationException();
-    }
+    Graph getGraphByBuildingId(Integer buildingId);
 }
