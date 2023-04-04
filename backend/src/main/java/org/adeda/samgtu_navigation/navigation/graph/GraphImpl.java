6ce95589ffea335e30fde0d16532ad5d9d8f4d3e
arsuhinars <arsuhinars@gmail.com>
2023-04-04 22:21:03 +0400

    :art: Removed useless ToString annotation in GraphImpl

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/GraphImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/graph/GraphImpl.java
@@ -1,13 +1,11 @@
 package org.adeda.samgtu_navigation.navigation.graph;
 
-import lombok.ToString;
 import org.adeda.samgtu_navigation.core.exception.InvalidFormatException;
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
 import org.adeda.samgtu_navigation.navigation.model.NavNode;
 
 import java.util.*;
 
-@ToString
 public class GraphImpl implements Graph {
     private final Set<NavNode> nodes;
     private final DistanceMatrix matrix;
