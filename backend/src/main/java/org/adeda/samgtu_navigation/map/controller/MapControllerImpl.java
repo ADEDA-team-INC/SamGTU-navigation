86125eadf8c4fc05923b5436555ca9e833e06eac
arsuhinars <arsuhinars@gmail.com>
2023-04-04 22:20:10 +0400

    :art: Added new methods in MapController due to API schema update

--- backend/src/main/java/org/adeda/samgtu_navigation/map/controller/MapControllerImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/controller/MapControllerImpl.java
@@ -1,23 +1,38 @@
 package org.adeda.samgtu_navigation.map.controller;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
 import org.adeda.samgtu_navigation.map.schema.MapBuildingSchema;
 import org.adeda.samgtu_navigation.map.schema.MapDomainSchema;
+import org.adeda.samgtu_navigation.map.schema.OutdoorObjectSchema;
 import org.adeda.samgtu_navigation.map.service.MapService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MimeTypeUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(
     path = "/map",
     produces = MimeTypeUtils.APPLICATION_JSON_VALUE
 )
+@Validated
 public class MapControllerImpl implements MapController {
     private final MapService service;
 
     public MapControllerImpl(MapService service) {
         this.service = service;
+    }
+
+    @Override
+    @GetMapping(path = "/buildings")
+    public List<MapBuildingSchema> getAllMapBuildings(
+        @RequestParam(defaultValue = "10") @Min(1) int size,
+        @RequestParam(defaultValue = "0") @Min(0) int page
+    ) {
+        return service.getAllBuildings(size, page);
     }
 
     @Override
@@ -44,5 +59,14 @@ public class MapControllerImpl implements MapController {
         }
 
         return domain.get();
+    }
+
+    @Override
+    @GetMapping(path = "/outdoor_objects")
+    public List<OutdoorObjectSchema> getAllOutdoorObjects(
+        @RequestParam(defaultValue = "10") @Min(1) int size,
+        @RequestParam(defaultValue = "0") @Min(0) int page
+    ) {
+        return service.getAllOutdoorObjects(size, page);
     }
 }
