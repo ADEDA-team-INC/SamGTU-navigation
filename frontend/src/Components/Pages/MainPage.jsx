5ff87748b6b6ad1fa6bf6fb9b59a5876e56b04f6
oliceglad <vycheslavofficial@gmail.com>
2023-04-05 14:34:47 +0400

    fix bugs and added background image to canvas FabricJs

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
+import { fabric } from 'fabric'
 
 import { Infobar } from '../Infobar/Infobar'
 import { Domain } from '../UI/domains/domain'
@@ -9,18 +9,32 @@ import initialDetails from '../../data/initialDetails';
 
 export const MainPage = props => {
 
-    const { editor, onReady } = useFabricJSEditor()
-
-    const onAddMap = () =>{
-        editor.addCircle()
+    const onAddMap = (rect) => {
+        canvas.add(rect);
+        console.log('privet')
     }
+
+    useEffect(() => {
+        const canvas = new fabric.Canvas('map', {
+            backgroundImage: 'https://img3.akspic.ru/previews/7/4/2/8/6/168247/168247-kosti_3d-igra_v_kosti_3d-azartnaya_igra-pitevaya_igra-kazino-500x.jpg'
+        })
+        const rect = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: 'red',
+            width: 20,
+            height: 20
+        });
+        canvas.add(rect);
+        onAddMap;
+    }, [])
 
     return (
         <div className='main'>
-            <FabricJSCanvas className="sample-canvas" onReady={onReady} />
+            <canvas id='map'></canvas>
             <div>
                 <div className='main__left__container'>
-                    <Search details={initialDetails} addMap ={onAddMap}/>
+                    <Search details={initialDetails} addMap={onAddMap} />
                 </div>
                 <div className='main__top__container'>
                     <Infobar />
