82fd00c22d5fa674cb8b7cd9f72c9bb9f89848a7
mister-mamaster <burlakop@gmail.com>
2023-04-05 22:26:08 +0400

    Empty GraphService implementation

---
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphServiceImpl.java
@@ -0,0 +1,11 @@
+package org.adeda.samgtu_navigation.navigation.service;
+
+import org.adeda.samgtu_navigation.navigation.graph.Graph;
+
+public class GraphServiceImpl implements GraphService{
+
+    @Override
+    public Graph getGraphByBuildingId(Integer buildingId) {
+        throw new UnsupportedOperationException();
+    }
+}
