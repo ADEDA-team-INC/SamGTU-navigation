3211a019ee4d80c42384b008b7b759e1e337c645
mister-mamaster <burlakop@gmail.com>
2023-04-08 18:10:09 +0400

    NavigationServiceImpl fix

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/NavigationServiceImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/NavigationServiceImpl.java
@@ -2,6 +2,8 @@ package org.adeda.samgtu_navigation.navigation.service;
 
 import org.adeda.samgtu_navigation.core.exception.InvalidFormatException;
 import org.adeda.samgtu_navigation.core.exception.NotFoundException;
+import org.adeda.samgtu_navigation.map.model.MapObject;
+import org.adeda.samgtu_navigation.map.repository.MapObjectRepository;
 import org.adeda.samgtu_navigation.navigation.graph.GraphImpl;
 import org.adeda.samgtu_navigation.navigation.model.NavNode;
 import org.adeda.samgtu_navigation.navigation.schema.NavPointSchema;
@@ -12,62 +14,58 @@ import java.util.*;
 @Service
 public class NavigationServiceImpl implements NavigationService{
 
+    MapObjectRepository mapObjectRepository;
+
+    GraphService graphService;
+
+    public NavigationServiceImpl(MapObjectRepository mapObjectRepository, GraphService graphService) {
+        this.mapObjectRepository = mapObjectRepository;
+        this.graphService = graphService;
+    }
+
     @Override
     public List<NavPointSchema> findPath(List<NavPointSchema> points) throws NotFoundException, InvalidFormatException {
-        GraphImpl graph = (GraphImpl) new GraphServiceImpl().getGraphByBuildingId(points.get(0).getMapObjectId());
+        List<MapObject> pointsMapObjects = points.stream().map(p -> mapObjectRepository.findByObjectId(p.getMapObjectId())).toList();
 
-        if(points.size() < 2) throw new InvalidFormatException("Less than 2 navPointSchema are specified");
+        GraphImpl graph = (GraphImpl) graphService.getGraphByBuildingId(
+                pointsMapObjects.get(0).getDomain().getBuilding().getId()
+        );
 
         //Проверка точек на принадлежность одному зданию
         for(int i = 1; i < points.size() - 1; i++){
-            if(!points.get(i).getMapObjectId().equals(points.get(0).getMapObjectId())) throw new InvalidFormatException("Points from different buildings");
+            if(!pointsMapObjects.get(i).getDomain().getBuilding().getId().equals
+                    (pointsMapObjects.get(0).getDomain().getBuilding().getId()))
+                throw new InvalidFormatException("Points from different buildings");
         }
 
-        Set<NavNode> nodesSet = new HashSet<>();
-        for(NavPointSchema point: points){
-            nodesSet.add(findNode(point, graph));
-        }
-
-        //Проверка на расположение всех точек в одном месте
-        if(nodesSet.size() <= 1){
-            throw new InvalidFormatException("Pathfinding is not need");
-        }
-
-        List<NavPointSchema> response = new ArrayList<>();
+        List<NavNode> nodeList = points.stream().map(p -> findClosestNode(p, mapObjectRepository.findByObjectId(p.getMapObjectId()).getNodes().stream().toList())).toList();
+        List<NavPointSchema> response = new LinkedList<>();
 
         //Прокладывание пути
-        for (int i = 0; i < nodesSet.size() - 1; i++){
-            for(NavNode node: graph.findPath
-                    (nodesSet.stream().toList().get(i),
-                    nodesSet.stream().toList().get(i + 1)))
-            {
-                response.add(new NavPointSchema(node));
-            }
+        for (int i = 0; i < nodeList.size() - 1; i++){
+            response.addAll(graph.findPath
+                    (nodeList.get(i), nodeList.get(i + 1))
+                    .stream().map(NavPointSchema::new).toList());
+
         }
 
         return response;
     }
 
-    private NavNode findNode(NavPointSchema point, GraphImpl graph){
-        List<NavNode> sameBuildingNodes = new ArrayList<>();
-
-        for(NavNode vert: graph.getNodes()){
-            if(vert.getMapObject().getId().equals(point.getMapObjectId())) sameBuildingNodes.add(vert);
-        }
-
-        if(sameBuildingNodes.size() == 0)
+    private NavNode findClosestNode(NavPointSchema point, List<NavNode> sameObjectNodes){
+        if(sameObjectNodes.size() == 0)
             throw new InvalidFormatException("This object(s) does not exist");
 
-        if(sameBuildingNodes.size() == 1)
-            return sameBuildingNodes.get(0);
+        if(sameObjectNodes.size() == 1)
+            return sameObjectNodes.get(0);
 
         //Поиск ближайшей точки
-        NavNode favouriteNode = sameBuildingNodes.get(0);
-        for(int i = 1; i < sameBuildingNodes.size(); i++){
-            if(Math.abs(point.getPositionX() - favouriteNode.getPosition().getX()) + Math.abs(point.getPositionY() - favouriteNode.getPosition().getY())
+        NavNode favouriteNode = sameObjectNodes.get(0);
+        for(int i = 1; i < sameObjectNodes.size(); i++){
+            if(favouriteNode.getPosition().subtract(point.getPositionX(), point.getPositionY()).length()
                     >
-               Math.abs(point.getPositionX() - sameBuildingNodes.get(i).getPosition().getX()) + Math.abs(point.getPositionY() - sameBuildingNodes.get(i).getPosition().getY())){
-                favouriteNode = sameBuildingNodes.get(i);
+                    sameObjectNodes.get(i).getPosition().subtract(point.getPositionX(), point.getPositionY()).length()){
+                favouriteNode = sameObjectNodes.get(i);
             }
         }
 
