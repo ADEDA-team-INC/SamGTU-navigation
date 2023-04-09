2c2f80e7f439507abadb060e122cb9e6274c9af9
arsuhinars <arsuhinars@gmail.com>
2023-04-09 17:32:00 +0400

    🎨 Reorganised views components

--- frontend/src/views/DesktopView.vue
+++ frontend/src/views/DesktopView.vue
@@ -1,9 +1,24 @@
 <template>
+    <div class="vw-100 vh-100 d-flex">
+        <aside class="border-end bg-light d-flex flex-column p-4">
+            <SearchBar />
+            <hr class="my-4">
+            <router-view />
+        </aside>
+        <main class="flex-grow-1">
 
+        </main>
+    </div>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
+@import "../scss/defines";
+
+aside {
+    width: $sidebar-width;
+}
 </style>
 
 <script setup lang="ts">
+import SearchBar from '../components/SearchBar.vue'
 </script>
