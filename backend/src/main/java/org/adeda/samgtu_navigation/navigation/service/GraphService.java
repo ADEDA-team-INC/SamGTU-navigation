c3b89aa4d3ca17f56021bfac3539c469efb9ef78
arsuhinars <arsuhinars@gmail.com>
2023-04-09 17:41:08 +0400

    :sparkles: Implemented GraphService

--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphService.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphService.java
@@ -1,12 +1,14 @@
 package org.adeda.samgtu_navigation.navigation.service;
 
+import org.adeda.samgtu_navigation.core.exception.NotFoundException;
 import org.adeda.samgtu_navigation.navigation.graph.Graph;
 
 public interface GraphService {
     /**
      * Метод для поставки графа, соответствующему одному зданию на карте.
-     * @param buildingId идентификатор здания
+     * @param id идентификатор здания
      * @return Новый объект графа, созданный с помощью `GraphFactory`, либо уже существующий.
+     * @throws NotFoundException здания с `id` не существует
      */
-    Graph getByBuildingId(Integer buildingId);
+    Graph getByBuildingId(Integer id) throws NotFoundException;
 }
--- backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphServiceImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/navigation/service/GraphServiceImpl.java
@@ -1,11 +1,43 @@
 package org.adeda.samgtu_navigation.navigation.service;
 
+import org.adeda.samgtu_navigation.core.exception.NotFoundException;
+import org.adeda.samgtu_navigation.map.repository.MapBuildingRepository;
 import org.adeda.samgtu_navigation.navigation.graph.Graph;
+import org.adeda.samgtu_navigation.navigation.graph.GraphFactory;
+import org.adeda.samgtu_navigation.navigation.repository.NodeRepository;
 
-public class GraphServiceImpl implements GraphService{
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphServiceImpl implements GraphService {
+    private final Map<Integer, Graph> graphByBuildingId;
+    private final GraphFactory graphFactory;
+    private final MapBuildingRepository buildingRepository;
+    private final NodeRepository nodeRepository;
+
+    public GraphServiceImpl(
+        MapBuildingRepository buildingRepository,
+        GraphFactory graphFactory,
+        NodeRepository nodeRepository
+    ) {
+        this.graphByBuildingId = new HashMap<>();
+        this.graphFactory = graphFactory;
+        this.buildingRepository = buildingRepository;
+        this.nodeRepository = nodeRepository;
+    }
 
     @Override
-    public Graph getByBuildingId(Integer buildingId) {
-        throw new UnsupportedOperationException();
+    public Graph getByBuildingId(Integer id) {
+        if (!graphByBuildingId.containsKey(id)) {
+            if (!buildingRepository.existsById(id)) {
+                throw new NotFoundException("MapBuilding with given id doesn't exist");
+            }
+
+            var nodes = nodeRepository.findByBuildingId(id);
+            var graph = graphFactory.createGraph(nodes);
+
+            graphByBuildingId.put(id, graph);
+        }
+        return graphByBuildingId.get(id);
     }
 }
