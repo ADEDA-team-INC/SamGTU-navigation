567b9fd04ea922630421039eb34ae1bb96c21780
arsuhinars <arsuhinars@gmail.com>
2023-04-09 12:04:34 +0400

    :fire: Removed useless method in MapObjectRepository

--- backend/src/main/java/org/adeda/samgtu_navigation/map/repository/MapObjectRepository.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/repository/MapObjectRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MapObjectRepository extends CrudRepository<MapObject, Integer> {
-    MapObject findByObjectId(Integer id);
 }
