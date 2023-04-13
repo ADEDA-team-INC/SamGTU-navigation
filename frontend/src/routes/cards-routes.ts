04e907a4d878df712174b52f354724ac3689b35c
arsuhinars <arsuhinars@gmail.com>
2023-04-13 10:49:30 +0400

    🎨 Routes update

--- frontend/src/routes/cards-routes.ts
+++ frontend/src/routes/cards-routes.ts
@@ -1,15 +1,17 @@
 import { RouteRecordRaw } from 'vue-router'
 import ObjectsCard from '../components/cards/ObjectsCard.vue'
-import MapBuildingCard from '../components/cards/MapBuildingCard.vue'
-import MapDomainCard from '../components/cards/MapDomainCard.vue'
+import BuildingListCard from '../components/cards/BuildingsListCard.vue'
+import BuildingCard from '../components/cards/BuildingCard.vue'
+import DomainCard from '../components/cards/DomainCard.vue'
 import MapObjectCard from '../components/cards/MapObjectCard.vue'
 import OutdoorObjectCard from '../components/cards/OutdoorObjectCard.vue'
 
 const routes: RouteRecordRaw[] = [
     { path: '/', component: ObjectsCard },
-    { path: '/building/:id', component: MapBuildingCard },
-    { path: '/building/:building_id/domain/:domain_id', component: MapDomainCard },
-    { path: '/building/:building_id/domain/:domain_id/object/:object_id', component: MapObjectCard },
+    { path: '/buildings', component: BuildingListCard, },
+    { path: '/building/:id', component: BuildingCard },
+    { path: '/domain/:domain_id', component: DomainCard },
+    { path: '/object/:object_id', component: MapObjectCard },
     { path: '/outdoor_object/:id', component: OutdoorObjectCard },
 ]
 
