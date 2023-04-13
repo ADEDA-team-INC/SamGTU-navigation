29f146c81562aa2fc2a651124a1fc8bb0063bf3c
arsuhinars <arsuhinars@gmail.com>
2023-04-13 23:29:22 +0400

    :art: Updated methods in MapService interface

--- backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapService.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapService.java
@@ -2,7 +2,9 @@ package org.adeda.samgtu_navigation.map.service;
 
 import org.adeda.samgtu_navigation.map.model.MapBuilding;
 import org.adeda.samgtu_navigation.map.model.MapDomain;
+import org.adeda.samgtu_navigation.map.model.MapObject;
 import org.adeda.samgtu_navigation.map.model.OutdoorObject;
+import org.adeda.samgtu_navigation.map.schema.MapSearchResult;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,22 +16,29 @@ public interface MapService {
      * @param page номер страницы
      * @return Список всех зданий с учетом текущей страницы
      */
-    List<MapBuilding> getAllBuildings(int size, int page);
+    List<MapBuilding> getBuildings(int size, int page);
 
     /**
      * Получения объекта здания на карте
      * @param id идентификатор здания
      *
-     * @return Объект здания `MapBuildingSchema`.
+     * @return Объект здания `MapBuilding`.
      */
     Optional<MapBuilding> getBuildingById(Integer id);
 
     /**
      * Получения объекта пространства на карте
      * @param id идентификатор пространства
-     * @return Объект пространства `MapDomainSchema`.
+     * @return Объект пространства `MapDomain`.
      */
     Optional<MapDomain> getDomainById(Integer id);
+
+    /**
+     * Получение объекта на карте по его идентификатору
+     * @param id идентификатор объекта
+     * @return Объект на карте `MapObject`.
+     */
+    Optional<MapObject> getObjectById(Integer id);
 
     /**
      * Получить все уличные объекты на карте. Поддерживает пагинацию
@@ -37,5 +46,13 @@ public interface MapService {
      * @param page номер страницы
      * @return Список всех уличных объектов с учетом текущей страницы
      */
-    List<OutdoorObject> getAllOutdoorObjects(int size, int page);
+    List<OutdoorObject> getOutdoorObjects(int size, int page);
+
+    /**
+     * Метод поиска сущностей на карте по запросу. Поиск идет по вхождению запроса в отображаемое имя или описание
+     * объектов.
+     * @param query текстовая строка запроса
+     * @return Результат поиска для каждого отдельного вида сущности (MapBuilding, MapObject, OutdoorObject)
+     */
+    MapSearchResult search(String query);
 }
--- backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapServiceImpl.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/map/service/MapServiceImpl.java
@@ -2,10 +2,13 @@ package org.adeda.samgtu_navigation.map.service;
 
 import org.adeda.samgtu_navigation.map.model.MapBuilding;
 import org.adeda.samgtu_navigation.map.model.MapDomain;
+import org.adeda.samgtu_navigation.map.model.MapObject;
 import org.adeda.samgtu_navigation.map.model.OutdoorObject;
 import org.adeda.samgtu_navigation.map.repository.MapBuildingRepository;
 import org.adeda.samgtu_navigation.map.repository.MapDomainRepository;
+import org.adeda.samgtu_navigation.map.repository.MapObjectRepository;
 import org.adeda.samgtu_navigation.map.repository.OutdoorObjectRepository;
+import org.adeda.samgtu_navigation.map.schema.MapSearchResult;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +19,23 @@ import java.util.Optional;
 public class MapServiceImpl implements MapService {
     private final MapBuildingRepository buildingRepository;
     private final MapDomainRepository domainRepository;
+    private final MapObjectRepository mapObjectRepository;
     private final OutdoorObjectRepository outdoorObjectRepository;
 
     public MapServiceImpl(
         MapBuildingRepository buildingRepository,
         MapDomainRepository domainRepository,
+        MapObjectRepository mapObjectRepository,
         OutdoorObjectRepository outdoorObjectRepository
     ) {
         this.buildingRepository = buildingRepository;
         this.domainRepository = domainRepository;
+        this.mapObjectRepository = mapObjectRepository;
         this.outdoorObjectRepository = outdoorObjectRepository;
     }
 
     @Override
-    public List<MapBuilding> getAllBuildings(int size, int page) {
+    public List<MapBuilding> getBuildings(int size, int page) {
         return buildingRepository.findAll(PageRequest.of(page, size)).toList();
     }
 
@@ -44,7 +50,17 @@ public class MapServiceImpl implements MapService {
     }
 
     @Override
-    public List<OutdoorObject> getAllOutdoorObjects(int size, int page) {
+    public Optional<MapObject> getObjectById(Integer id) {
+        return mapObjectRepository.findById(id);
+    }
+
+    @Override
+    public List<OutdoorObject> getOutdoorObjects(int size, int page) {
         return outdoorObjectRepository.findAll(PageRequest.of(page, size)).toList();
+    }
+
+    @Override
+    public MapSearchResult search(String query) {
+        throw new UnsupportedOperationException();
     }
 }
