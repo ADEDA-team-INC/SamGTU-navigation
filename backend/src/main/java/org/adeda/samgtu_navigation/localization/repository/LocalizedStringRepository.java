dc425509ef9c3ac7cb273b7adf9ce597225be730
arsuhinars <arsuhinars@gmail.com>
2023-04-15 23:32:36 +0400

    :fire: Removed useless method in LocalizedStringRepository

--- backend/src/main/java/org/adeda/samgtu_navigation/localization/repository/LocalizedStringRepository.java
+++ backend/src/main/java/org/adeda/samgtu_navigation/localization/repository/LocalizedStringRepository.java
@@ -1,22 +1,13 @@
 package org.adeda.samgtu_navigation.localization.repository;
 
-import jakarta.persistence.Tuple;
 import org.adeda.samgtu_navigation.localization.enums.SupportedLanguage;
 import org.adeda.samgtu_navigation.localization.model.LocalizedString;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface LocalizedStringRepository extends CrudRepository<LocalizedString, Integer> {
     @Query("SELECT str FROM LocalizedString str WHERE str.key = ?1 AND str.language = ?2")
     Optional<LocalizedString> findByKey(String key, SupportedLanguage language);
-
-    @Query("""
-        SELECT str.key AS key, str AS string
-        FROM LocalizedString str WHERE
-            str.language = ?2 AND str.key IN ?1
-        """)
-    List<Tuple> findByKeys(List<String> keys, SupportedLanguage language);
 }
