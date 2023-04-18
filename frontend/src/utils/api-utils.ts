6d78841d221d8731ddb1f1d6b02f8b999e220db3
arsuhinars <arsuhinars@gmail.com>
2023-04-18 11:10:21 +0400

    ✨ Added api-utils module

---
+++ frontend/src/utils/api-utils.ts
@@ -0,0 +1,31 @@
+import defines from '../defines'
+import { useUserStore } from '../stores/user-store'
+
+async function get(path: string, params: { [key: string]: string | null }) {
+    const userStore = useUserStore()
+
+    let url = new URL(path, defines.BASE_URL)
+    for (const [key, value] of Object.entries(params)) {
+        if (typeof value === 'string') {
+            url.searchParams.append(key, value)
+        }
+    }
+
+    let options = {
+        headers: {
+            'Accept-Language': userStore.language
+        }
+    }
+
+    let result = await fetch(url, options)
+
+    if (!result.ok) {
+        throw new Error(`Error occured while fetching ${path} with status code ${result.status}`)
+    }
+
+    return await result.json()
+}
+
+export default {
+    get
+}
