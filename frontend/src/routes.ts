523bc7aeb98aa06d214e277459313adb1de0bb2a
arsuhinars <arsuhinars@gmail.com>
2023-04-20 14:58:01 +0400

    ✨ Added OutdoorObjectsList component

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,10 +1,11 @@
 import { RouteRecordRaw } from 'vue-router'
-import ObjectsCard from './views/cards/ObjectsCard.vue'
-import BuildingListCard from './views/cards/BuildingsListCard.vue'
+import ObjectsCard from './views/cards/MainList.vue'
+import BuildingListCard from './views/cards/BuildingsList.vue'
 import BuildingCard from './views/cards/BuildingCard.vue'
 import DomainCard from './views/cards/DomainCard.vue'
 import MapObjectCard from './views/cards/MapObjectCard.vue'
 import OutdoorObjectCard from './views/cards/OutdoorObjectCard.vue'
+import OutdoorObjectsList from './views/cards/OutdoorObjectsList.vue'
 
 const routes: RouteRecordRaw[] = [
     { path: '/', component: ObjectsCard },
@@ -12,6 +13,7 @@ const routes: RouteRecordRaw[] = [
     { path: '/building/:id', component: BuildingCard },
     { path: '/domain/:domain_id', component: DomainCard },
     { path: '/object/:object_id', component: MapObjectCard },
+    { path: '/outdoor_objects', component: OutdoorObjectsList },
     { path: '/outdoor_object/:id', component: OutdoorObjectCard },
 ]
 
---
+++ frontend/src/views/cards/OutdoorObjectsList.vue
@@ -0,0 +1,21 @@
+<template>
+    <CardsList
+        :fetcher="page => fetchOutdoorObjects(objectType, page)"
+        :link-builder="item => `/outdoor_object/${item.id}`"
+    />
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { fetchOutdoorObjects } from '../../api/map-api';
+import CardsList from '../../components/CardsList.vue';
+import { OutdoorObjectType } from '../../schemas/map-schemas';
+
+const route = useRoute()
+
+let objectType: OutdoorObjectType | null = null
+if (typeof route.query['type'] === 'string') {
+    objectType = route.query.type as OutdoorObjectType
+}
+
+</script>
