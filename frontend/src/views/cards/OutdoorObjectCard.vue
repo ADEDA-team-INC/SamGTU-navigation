3ab6e40ec0da6dd9abf351cbe426347bd4b79ceb
arsuhinars <arsuhinars@gmail.com>
2023-04-21 18:22:08 +0400

    ✨ Implemented OutdoorObjectCard component

--- frontend/src/views/cards/OutdoorObjectCard.vue
+++ frontend/src/views/cards/OutdoorObjectCard.vue
@@ -1,9 +1,26 @@
 <template>
-
+    <Card :entity="outdoorObject"/>
 </template>
 
 <style scoped lang="scss">
 </style>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import Card from '../../components/Card.vue';
+import { onMounted, ref } from 'vue';
+import { fetchOutdoorObjectById } from '../../api/map-api';
+import { OutdoorObjectSchema } from '../../schemas/map-schemas';
+
+const outdoorObject = ref<OutdoorObjectSchema | null>(null)
+const route = useRoute()
+
+onMounted(async () => {
+    if (typeof route.params.id !== 'string') {
+        return
+    }
+
+    outdoorObject.value = await fetchOutdoorObjectById(Number.parseInt(route.params.id))
+})
+
 </script>
