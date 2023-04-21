de82728a8d3f0a835b0d5c052abd04b21617ca86
oliceglad <vycheslavofficial@gmail.com>
2023-04-21 21:27:29 +0400

    changed initialDetails and changed Search component

--- frontend/src/Components/Search/Card.jsx
+++ frontend/src/Components/Search/Card.jsx
@@ -5,9 +5,14 @@ import geo from '../../assets/img/GeoAltFill.svg';
 
 function Card({ building, addMap }) {
   return (
-    <div className={s.card} onClick={addMap}>
+    <div className={s.card} onClick={
+      () => {
+        console.log(building.cabinets)
+      }
+    }>
       <img className={s.card__geo} src={geo} />
       <div className={s.card__text}>
+        <img src={building.img} alt="fjdjfdjf" />
         <h3>{building.name}</h3>
         <span>{building.adress}</span>
       </div>
