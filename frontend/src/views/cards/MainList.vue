431bb55816e37fd0751a2c532919c7a4f196d195
arsuhinars <arsuhinars@gmail.com>
2023-04-21 18:22:28 +0400

    ✨ Added links to outdoor objects lists

--- frontend/src/views/cards/MainList.vue
+++ frontend/src/views/cards/MainList.vue
@@ -3,8 +3,15 @@
         <router-link class="unstyled-a" to="/buildings">
             <IconButton icon-class="bi-building-fill" subtitle="Корпуса" />
         </router-link>
-        <IconButton icon-class="bi-basket2-fill" subtitle="Магазины" />
-        <IconButton icon-class="bi-cup-hot-fill" subtitle="Кафе" />
+        <router-link class="unstyled-a" to="/outdoor_objects?type=SHOP">
+            <IconButton icon-class="bi-basket2-fill" subtitle="Магазины" />
+        </router-link>
+        <router-link class="unstyled-a" to="/outdoor_objects?type=CAFE">
+            <IconButton icon-class="bi-cup-hot-fill" subtitle="Кафе" />
+        </router-link>
+        <router-link class="unstyled-a" to="/outdoor_objects?type=SIGHT">
+            <IconButton icon-class="bi-bank2" subtitle="Места" />
+        </router-link>
     </div>
 </template>
 
