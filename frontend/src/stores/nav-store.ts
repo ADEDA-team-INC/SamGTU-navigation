267fe27aa520aba918efb83cb65b7af81c09866d
arsuhinars <arsuhinars@gmail.com>
2023-04-22 18:26:03 +0400

    🎨 Improved nav store

--- frontend/src/stores/nav-store.ts
+++ frontend/src/stores/nav-store.ts
@@ -1,5 +1,35 @@
+import { computed, ref, watch } from "vue";
 import { defineStore } from "pinia";
+import { MapBuildingSchema, MapDomainSchema } from "../schemas/map-schemas";
+import { fetchBuildingById, fetchDomainById } from "../api/map-api";
 
 export const useNavStore = defineStore('navStore', () => {
+    const buildingId = ref<number>(1)
+    const building = ref<MapBuildingSchema | null>(null)
+    const floor = ref<string>('')
+    const domain = ref<MapDomainSchema | null>(null)
 
+    watch(buildingId, async (newId, oldId) => {
+        try {
+            building.value = await fetchBuildingById(newId)
+            floor.value = Object.keys(building.value.domainIds)[0] ?? ''
+        } catch {
+            building.value = null
+        }
+    })
+
+    watch(floor, async (newFloor, oldFloor) => {
+        if (building.value === null || building.value.domainIds[newFloor] === null) {
+            domain.value = null
+            return
+        }
+
+        try {
+            domain.value = await fetchDomainById(building.value.domainIds[newFloor])
+        } catch {
+            domain.value = null
+        }
+    })
+
+    return { buildingId, building, floor, domain }
 })
