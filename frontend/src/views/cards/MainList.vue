ef69c782ce107a442d5287544b84bd9f989caa9a
arsuhinars <arsuhinars@gmail.com>
2023-04-26 16:10:56 +0400

    🎨 Improved MainList

--- frontend/src/views/cards/MainList.vue
+++ frontend/src/views/cards/MainList.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="d-flex flex-wrap justify-content-around gap-4">
-        <router-link class="unstyled-a" to="/buildings">
+        <router-link to="/buildings">
             <IconButton icon-class="bi-building-fill" subtitle="Корпуса" />
         </router-link>
-        <router-link class="unstyled-a" to="/outdoor_objects?type=SHOP">
+        <router-link to="/outdoor_objects?type=SHOP">
             <IconButton icon-class="bi-basket2-fill" subtitle="Магазины" />
         </router-link>
-        <router-link class="unstyled-a" to="/outdoor_objects?type=CAFE">
+        <router-link to="/outdoor_objects?type=CAFE">
             <IconButton icon-class="bi-cup-hot-fill" subtitle="Кафе" />
         </router-link>
-        <router-link class="unstyled-a" to="/outdoor_objects?type=SIGHT">
+        <router-link to="/outdoor_objects?type=SIGHT">
             <IconButton icon-class="bi-bank2" subtitle="Места" />
         </router-link>
     </div>
 </template>
+
+<style scoped lang="scss">
+@import "../../scss/style.scss";
+
+a {
+    @extend .unstyled-a;
+}
+</style>
 
 <script setup lang="ts">
 import IconButton from '../../components/IconButton.vue';
