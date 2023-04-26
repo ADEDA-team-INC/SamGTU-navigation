b8f7594b050f28ff5de516e344602a209cd7301a
arsuhinars <arsuhinars@gmail.com>
2023-04-26 16:11:12 +0400

    ✨ Implemented MapObjectCard

--- frontend/src/views/cards/MapObjectCard.vue
+++ frontend/src/views/cards/MapObjectCard.vue
@@ -1,9 +1,33 @@
 <template>
-
+    <Card :entity="mapObject">
+        <div>
+            <h4>Показать</h4>
+            <div class="d-flex justify-content-between">
+                <button type="button" class="btn btn-primary">
+                    <i class="bi bi-map-fill"></i>
+                    На карте
+                </button>
+            </div>
+        </div>
+    </Card>
 </template>
 
-<style scoped lang="scss">
-</style>
-
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import Card from '../../components/Card.vue';
+import { onMounted, ref } from 'vue';
+import { fetchMapObjectById } from '../../api/map-api';
+import {  DetailedMapObject } from '../../schemas/map-schemas';
+
+const mapObject = ref<DetailedMapObject | null>(null)
+const route = useRoute()
+
+onMounted(async () => {
+    if (typeof route.params.id !== 'string') {
+        return
+    }
+
+    mapObject.value = await fetchMapObjectById(Number.parseInt(route.params.id))
+})
+
 </script>
