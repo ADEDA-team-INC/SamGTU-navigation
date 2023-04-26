2985ac04153c066f358edb11a1b7562c7a2f1cef
arsuhinars <arsuhinars@gmail.com>
2023-04-26 11:07:29 +0400

    🎨 Improved SearchBar component

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,14 +1,41 @@
 <template>
-    <div class="d-flex gap-2">
-        <input class="form-control" type="text" placeholder="Поиск">
-        <button class="btn btn-primary">
+    <form class="d-flex gap-2" @submit.prevent="onSearchClick">
+        <input class="form-control" type="text" placeholder="Поиск" ref="searchInput">
+        <button type="submit" class="btn btn-primary">
             <i class="bi bi-search"></i>
         </button>
-    </div>
+    </form>
 </template>
 
 <style scoped lang="scss">
 </style>
 
 <script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute()
+const router = useRouter()
+
+let searchInput = ref<HTMLInputElement | null>(null)
+
+function onSearchClick() {
+    router.push({
+        name: 'search',
+        query: { query: searchInput.value?.value }
+    })
+}
+
+onMounted(() => {
+    watch(route, () => {
+        let searchQuery = ''
+        if (typeof route.query['query'] === 'string') {
+            searchQuery = route.query.query
+        }
+        if (searchInput.value !== null) {
+            searchInput.value.value = searchQuery
+        }
+    }, { immediate: true })
+})
+
 </script>
