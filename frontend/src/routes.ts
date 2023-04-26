463be883a287870259cc656da55ff1a7b64e86b6
arsuhinars <arsuhinars@gmail.com>
2023-04-26 16:09:38 +0400

    🎨 Improved routes

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,21 +1,19 @@
 import { RouteRecordRaw } from 'vue-router'
-import ObjectsCard from './views/cards/MainList.vue'
+import MainList from './views/cards/MainList.vue'
 import BuildingListCard from './views/cards/BuildingsList.vue'
 import BuildingCard from './views/cards/BuildingCard.vue'
-import DomainCard from './views/cards/DomainCard.vue'
 import MapObjectCard from './views/cards/MapObjectCard.vue'
 import OutdoorObjectCard from './views/cards/OutdoorObjectCard.vue'
 import OutdoorObjectsList from './views/cards/OutdoorObjectsList.vue'
 import SearchResultsVue from './views/cards/SearchResults.vue'
 
 const routes: RouteRecordRaw[] = [
-    { path: '/', component: ObjectsCard },
-    { path: '/buildings', component: BuildingListCard, },
-    { path: '/building/:id', component: BuildingCard },
-    { path: '/domain/:domain_id', component: DomainCard },
-    { path: '/object/:object_id', component: MapObjectCard },
-    { path: '/outdoor_objects', component: OutdoorObjectsList },
-    { path: '/outdoor_object/:id', component: OutdoorObjectCard },
+    { name: 'main', path: '/', component: MainList },
+    { name: 'map_buildings_list', path: '/buildings', component: BuildingListCard, },
+    { name: 'map_building', path: '/building/:id', component: BuildingCard },
+    { name: 'map_object', path: '/object/:id', component: MapObjectCard },
+    { name: 'outdoor_objects_list', path: '/outdoor_objects', component: OutdoorObjectsList },
+    { name: 'outdoor_object', path: '/outdoor_object/:id', component: OutdoorObjectCard },
     { name: 'search', path: '/search', component: SearchResultsVue }
 ]
 
