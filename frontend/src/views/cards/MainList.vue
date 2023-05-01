7415b607131c1f6f290144b29ba893ba78081415
arsuhinars <arsuhinars@gmail.com>
2023-05-01 14:52:08 +0400

    🎨 Improved MainList view

--- frontend/src/views/cards/MainList.vue
+++ frontend/src/views/cards/MainList.vue
@@ -1,29 +1,31 @@
 <template>
-    <div class="d-flex flex-wrap justify-content-around gap-4">
-        <router-link to="/buildings">
-            <IconButton
-                icon-class="bi-building-fill"
-                :subtitle="$t('main_page.buildings')"
-            />
-        </router-link>
-        <router-link to="/outdoor_objects?type=SHOP">
-            <IconButton
-                icon-class="bi-basket2-fill"
-                :subtitle="$t('main_page.shops')"
-            />
-        </router-link>
-        <router-link to="/outdoor_objects?type=CAFE">
-            <IconButton
-                icon-class="bi-cup-hot-fill"
-                :subtitle="$t('main_page.cafes')"
-            />
-        </router-link>
-        <router-link to="/outdoor_objects?type=SIGHT">
-            <IconButton
-                icon-class="bi-bank2"
-                :subtitle="$t('main_page.sights')"
-            />
-        </router-link>
+    <div class="container">
+        <div class="row row-cols-3">
+            <router-link class="col" to="/buildings">
+                <IconButton
+                    icon-class="bi-building-fill"
+                    :subtitle="$t('main_page.buildings')"
+                />
+            </router-link>
+            <router-link class="col" to="/outdoor_objects?type=SHOP">
+                <IconButton
+                    icon-class="bi-basket2-fill"
+                    :subtitle="$t('main_page.shops')"
+                />
+            </router-link>
+            <router-link class="col" to="/outdoor_objects?type=CAFE">
+                <IconButton
+                    icon-class="bi-cup-hot-fill"
+                    :subtitle="$t('main_page.cafes')"
+                />
+            </router-link>
+            <router-link class="col" to="/outdoor_objects?type=SIGHT">
+                <IconButton
+                    icon-class="bi-bank2"
+                    :subtitle="$t('main_page.sights')"
+                />
+            </router-link>
+        </div>
     </div>
 </template>
 
@@ -32,6 +34,10 @@
 
 a {
     @extend .unstyled-a;
+}
+
+.col:nth-child(n + 4) {
+    margin-top: map-get($spacers, 4);
 }
 </style>
 
