3e1030f5420b2d0a4da80e9bba271006d437b1ca
oliceglad <vycheslavofficial@gmail.com>
2023-05-06 20:23:00 +0400

    deleted Search component

--- frontend/src/Components/Pages/MainPage.jsx
+++ frontend/src/Components/Pages/MainPage.jsx
@@ -2,10 +2,7 @@ import React, { useEffect } from "react";
 
 import { Infobar } from "../Infobar/Infobar";
 import { Domain } from "../UI/domains/domain";
-import Search from "../Search/Search";
-import initialDetails from "../../data/initialDetails";
 import { useTranslation } from "react-i18next";
-import { mainAPI } from "../../api/api";
 import { requestBuildings } from "../../redux/mainReducer";
 import { connect } from 'react-redux'
 
@@ -25,14 +22,14 @@ const MainPage = ({ requestBuildings, buildings, isFetching}) => {
         <div className="main">
             <div>
                 <div className="main__left__container">
-                    <Search details={initialDetails} translation={t} setTranslation={i18n} />
+
                 </div>
                 <div className="main__top__container">
                     <Infobar translation={t} />
                 </div>
                 <div className="main__right__container">
                     <Domain />
-                    {/* <Zoom /> */}
+                    <Zoom />
                 </div>
             </div>
         </div>
